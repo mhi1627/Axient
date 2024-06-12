645d2e24b997201a7956dda76f9737f854c6cdcf
mhi1627 <pvg1627@gmail.com>
2024-06-12 19:25:59 +0530

    Axient Web

---
+++ script.js
@@ -0,0 +1,33 @@
+// let hover = gsap.to(".innerCard", {
+// top :0 
+// }) 
+// let card = document.querySelector(".service")
+// card.addEventListener("mousnter", () => hover.play());
+// card.addEventListener("mouse\eave", () => hover.reverse());
+
+
+
+<div class="sectionInfo">
+<div class="head">
+    <h1>ABOUT <br>US</h1>
+</div>
+
+</div>
+<!-- <div class="whatWedo">
+<h2>Axient Infotect help you in different areas.</h2>
+<div class="webDev">
+    <h2>WEB DEVELOPMENT</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
+    <img src="New folder/WEB ST02.svg" alt="">
+</div>
+<div class="staffing">
+    <h2>Staffing</h2>
+    <img src="New folder/saffing3.svg" alt="">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
+</div>
+<div class="businessIntelligence">
+    <h2>Business Intelligence</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
+    <img src="New folder/BI ST  G 7.svg ST02.svg" alt="">
+</div>
+</div> -->
