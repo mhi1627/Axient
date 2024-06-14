f4c0b2b5a623eb5f2fba32103e1320d5a23a6c55
mhi1627 <pvg1627@gmail.com>
2024-06-14 21:01:56 +0530

    add menu

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-// let hover = gsap.to(".innerCard", {
-// top :0 
-// }) 
-// let card = document.querySelector(".service")
-// card.addEventListener("mousnter", () => hover.play());
-// card.addEventListener("mouse\eave", () => hover.reverse());
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("menu-icon");
+    const menubox = document.getElementById("menubox");
+    const closeBtn = document.getElementById("close-btn");
+    const main = document.querySelector('.main');
 
-<div class="sectionInfo">
-<div class="head">
-    <h1>ABOUT <br>US</h1>
-</div>
+    // Toggle menu box on menu icon click
+    menuIcon.addEventListener("click", function() {
+        menubox.classList.toggle("open");
+        main.style.backgroundColor = "#ffffff81"
+    });
 
-</div>
-<!-- <div class="whatWedo">
-<h2>Axient Infotect help you in different areas.</h2>
-<div class="webDev">
-    <h2>WEB DEVELOPMENT</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
-    <img src="New folder/WEB ST02.svg" alt="">
-</div>
-<div class="staffing">
-    <h2>Staffing</h2>
-    <img src="New folder/saffing3.svg" alt="">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
-</div>
-<div class="businessIntelligence">
-    <h2>Business Intelligence</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, tempore.</p>
-    <img src="New folder/BI ST  G 7.svg ST02.svg" alt="">
-</div>
-</div> -->
+    // Close menu box on close button click
+    closeBtn.addEventListener("click", function() {
+        menubox.classList.remove("open");
+    });
+
+    // Close menu box when clicking outside of it
+    document.addEventListener("click", function(event) {
+        if (!menubox.contains(event.target) && event.target !== menuIcon) {
+            menubox.classList.remove("open");
+        }
+    });
+
+});
